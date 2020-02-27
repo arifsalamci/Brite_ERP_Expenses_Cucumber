40e7f3f3540cf6cb92a03272f402e5eea25978fa
IsmailovaB <56233988+IsmailovaB@users.noreply.github.com>
2020-02-26 22:00:55 -0600

    commit

--- src/test/java/pages/Expense_Reports_Page.java
+++ src/test/java/pages/Expense_Reports_Page.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+
 public class Expense_Reports_Page {
     public Expense_Reports_Page(){
         PageFactory.initElements(Driver.getDriver(),this);
